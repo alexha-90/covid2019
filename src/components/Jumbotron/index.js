4bcc94a1b35515737e10dd05c9689c44dbeea695
Alex Ha <alex.ha@volvocars.com>
2020-03-14 16:35:38 -0700

    Begin locator template

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Button from 'react-bootstrap/Button';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 
 import Carousel from "./Carousel";
@@ -9,16 +8,13 @@ import "./style.scss"
 
 const JumbotronComponent = () => {
   return (
-    <Jumbotron className="headline-container">
+    <Jumbotron className="jumbotron-container">
       <h1>COVID 2019-2020</h1>
       <p>
         The first step to mitigation is understanding.
         Find and read reviews of COVID-19 testing centers near you...
       </p>
       <Carousel />
-      <p>
-        <Button variant="primary">Learn more</Button>
-      </p>
     </Jumbotron>
   )
 };
