37d05f86ea97020e1fde099d8da449485e1b91ec
Alex Ha <alexh.90@live.com>
2020-03-15 03:21:19 -0700

    Run prettier linting

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -1,1 +1,2 @@
-export const NEWS_API = "http://newsapi.org/v2/top-headlines?pageSize=10&country=us&q=coronavirus&apiKey=3642a3949e0c422e9bbd50fd9ce536bc";
+export const NEWS_API =
+  'http://newsapi.org/v2/top-headlines?pageSize=10&country=us&q=coronavirus&apiKey=3642a3949e0c422e9bbd50fd9ce536bc';
