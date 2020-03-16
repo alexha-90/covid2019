974842520437ef9ae714b14abc62b28d0abca19c
Alex Ha <alexh.90@live.com>
2020-03-16 00:08:51 -0700

    Change font. Fix overscrolling bug

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import createHistory from 'history/createBrowserHistory'
-import { Switch, Route, Router } from "react-router";
 
 import Header from "./components/Header";
-import Jumbotron from "./components/Jumbotron";
 import Statistics from "./components/Statistics";
 import Reviews from "./components/Reviews";
 import News from "./components/News";
@@ -12,24 +9,35 @@ import Footer from "./components/Footer";
 import './App.scss';
 //============================================================================//
 
+/*
+// unused. Should be considered as/if app grows in complexity
+
+import createHistory from 'history/createBrowserHistory'
+import { Switch, Route, Router } from "react-router";
+import Jumbotron from "./components/Jumbotron";
+
 const history = createHistory();
+<Router history={history} >
+  <Switch>
+    <Route exact path="/" component={Header} />
+    pass props here
+    <Jumbotron />
+  </Switch>
+</Router>
+*/
+
 
 function App() {
   return (
-    <div className="app-container">
-      <Router history={history} >
-        <Header />
-        {/*<Switch>*/}
-          {/*<Route exact path="/" component={Header} />*/}
-          {/*pass props here*/}
-          {/*<Jumbotron />*/}
-          <Statistics />
-          <Reviews />
-          <hr />
-          <News />
-        {/*</Switch>*/}
-        <Footer />
-      </Router>
+    <div>
+      <Header />
+      <div className="app-container">
+        <Statistics />
+        <Reviews />
+        <hr />
+        <News />
+      </div>
+      <Footer />
     </div>
   );
 }
